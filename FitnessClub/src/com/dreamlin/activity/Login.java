0268d6322cebddf35156f7146f585a8559e9349d
lin761222 <alex.chen1222@gmail.com>
2014-12-05 01:01:56 +0800

    SharedPerferences

--- FitnessClub/src/com/dreamlin/activity/Login.java
+++ FitnessClub/src/com/dreamlin/activity/Login.java
@@ -3,7 +3,10 @@ package com.dreamlin.activity;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +17,10 @@ public class Login extends Activity {
 	private EditText mUser; // 帳號編輯框
 	private EditText mPassword; // 密碼編輯框
 
+	final String PREFS_NAME = "ACCOUNT";// 喜好設定名稱
+	final String User = "User";
+	final String Password = "Password";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,9 +28,25 @@ public class Login extends Activity {
 
 		mUser = (EditText) findViewById(R.id.login_user_edit);
 		mPassword = (EditText) findViewById(R.id.login_passwd_edit);
+
+		// 取得SharedPerferences物件
+		SharedPreferences sp = getSharedPreferences(PREFS_NAME,
+				Context.MODE_PRIVATE);
+
+		mUser.setText(sp.getString(User, ""));
+		mPassword.setText(sp.getString(Password, ""));
 	}
 
 	public void login_mainweixin(View v) {
+		SharedPreferences sp = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+		Editor editor = sp.edit(); // 取得編輯器物件
+		String user = mUser.getText() + "";
+		editor.putString(User, user);
+		String password = mPassword.getText() + "";
+		editor.putString(Password, password);
+		editor.commit();
+
 		if ("alex".equals(mUser.getText().toString())
 				&& "123".equals(mPassword.getText().toString())) // 判斷帳號和密碼
 		{
@@ -49,8 +72,9 @@ public class Login extends Activity {
 					.create().show();
 		}
 	}
-	public void login1(View v){
-		Intent it = new Intent(Login.this,MainApp.class);
+
+	public void login1(View v) {
+		Intent it = new Intent(Login.this, MainApp.class);
 		startActivity(it);
 	}
 }
